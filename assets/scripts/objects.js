a18b644694802e3decc2534bb63aa6e84a9ca02b
shahin1290 <s.patowary@yahoo.com>
2020-09-05 12:07:57 +0200

    Adds logic to loop in array and object to show dynamic value of object

--- assets/scripts/objects.js
+++ assets/scripts/objects.js
@@ -13,9 +13,18 @@ const renderMovies = () => {
   }
   //movieList.innerHTML = ''
 
+
   movies.forEach((movie) => {
     const movieEl = document.createElement('li')
-    movieEl.textContent = movie.info.title
+
+    let text = movie.info.title + '-'
+
+    for(const key in movie.info){
+      if( key != 'title'){
+        text = text + `${key}: ${movie.info[key]}`
+      }
+    }
+    movieEl.textContent = text
     movieList.append(movieEl)
   })
 
@@ -25,8 +34,6 @@ const addMovieHandler = () => {
   const title = document.getElementById('title').value
   const extraName = document.getElementById('extra-name').value
   const extraValue = document.getElementById('extra-value').value
-
-  
 
   if (
     title.trim() === '' ||
